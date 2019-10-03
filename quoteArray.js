19d05e598f5465c243df9667a5222a2dae6ade96
Peterturn <55192090+Peterturn@users.noreply.github.com>
2019-10-03 15:35:28 +0200

    Add files via upload

---
+++ quoteArray.js
@@ -0,0 +1,63 @@
+/*
+In this project, you'll be tackling three main things:
+
+1. Build a collection of quotations — these are the quotes, author and other information that gets printed to the screen. You'll do this by creating a JavaScript array containing JavaScript objects — one object for each quotation.
+2. Create a function that pulls one of those quotes out at random.
+3. Create a function that prints that quote to the screen.
+
+*/
+var quotations =[
+  {
+  author: 'Peppa Pig' ,
+    quote:'Everyone loves jumping up and down in muddy puddles!'
+  },
+  {
+  author: 'King David Benjessi, ' ,
+    quote:'God is our refuge and strength, an ever-present help in trouble. Psalm 46:1-3'
+
+  },
+  {
+    author: 'Steve Jobs' ,
+      quote:'Design is not just what it looks like and feels like. Design is how it works. Innovation distinguishes between a leader and a follower. I want to put a ding in the universe.'
+  },
+  {
+    author: 'Bill Gates' ,
+      quote:'Success is a lousy teacher. It seduces smart people into thinking they can’t lose.'
+  },
+  {
+    author: 'Mother Teresa' ,
+      quote:'Spread love everywhere you go. Let no one ever come to you without leaving happier. Kind words can be short and easy to speak, but their echoes are truly endless.'
+  },
+  {
+    author: 'Bob Marley' ,
+      quote:'The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.'
+  },
+  {
+    author: 'Jesus Christ' ,
+      quote:'For what shall it profit a man, if he gain the whole world, and suffer the loss of his soul?'
+  },
+  {
+    author: 'David Hasselhoff' ,
+      quote:'I am a cheesy over-the-top megalomaniac with a deep voice and the most amazing pecs.'
+  },
+  {
+    author: 'George W. Bush' ,
+      quote:'Some folks look at me and see a certain swagger, which in Texas is called "walking."'
+  },
+  {
+    author: 'Donald Trump' ,
+      quote:'No dream is too big. No challenge is too great. Nothing we want for our future is beyond our reach.'
+  },
+  {
+    author: 'Britney Spears' ,
+      quote:'The cool thing about being famous is traveling. I have always wanted to travel across seas, like to Canada and stuff.'
+  },
+  {
+    author: 'Adolf Hitler' ,
+      quote:'He alone, who owns the youth, gains the future.'
+  },
+  {
+    author: 'Barack Obama' ,
+      quote: 'If you are walking down the right path and you are willing to keep walking, eventually you will make progress.'
+  }
+];
