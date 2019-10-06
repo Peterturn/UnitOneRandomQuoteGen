0f4e2f151064d84c811201f4471e06af8a71d17e
Peterturn31 <55192090+Peterturn@users.noreply.github.com>
2019-10-06 12:04:44 +0200

    finishing touches to the code.
    worked on random color function and the interval function as well.

--- js/script.js
+++ js/script.js
@@ -3,158 +3,159 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
 /*
 quotes selected at random from different affiliations, political parties, celeberities,
 religouse leaders, and television series. Quotes are picked in regard to positive wording and not
 according to any political or religious views they might hold.
-
 */
 
+//Vars for the random color function named: 'Function prettyColors()' near end of JS document.
+var red;
+var blue;
+var green;
+var randomColor;
+
+//Part one of coding project
+//Object array to hold quotes for random quote generator.
 var quotes =[
   {
     //Quote 1
-  author: 'Peppa Pig' ,
+  source: 'Peppa Pig' ,
     quote:'Everyone loves jumping up and down in muddy puddles!',
     citation: 'The Peppa Pig Series',
     tag:'television'
   },
   {
     //Quote 2
-  author: 'King David Benjessi, ' ,
+  source: 'King David Benjessi, ' ,
     quote:'God is our refuge and strength, an ever-present help in trouble.',
     citation: 'NKJV Bible, Psalm 46:1-3',
-    number:'religious'
+    tag:'religious'
 
 
   },
   {
     //Quote 3
-    author: 'Steve Jobs' ,
+    source: 'Steve Jobs' ,
       quote:'Design is not just what it looks like and feels like. Design is how it works. Innovation distinguishes between a leader and a follower. I want to put a ding in the universe.',
-      number:'business'
+      tag:'business'
   },
   {
     //Quote 4
-    author: 'Bill Gates' ,
+    source: 'Bill Gates' ,
       quote:'Success is a lousy teacher. It seduces smart people into thinking they can’t lose.',
-      number:'business'
+      tag:'business'
   },
   {
     //Quote 5
-    author: 'Mother Teresa' ,
+    source: 'Mother Teresa' ,
       quote:'Spread love everywhere you go. Let no one ever come to you without leaving happier. Kind words can be short and easy to speak, but their echoes are truly endless.',
-      number:'religious'
+      tag:'religious'
   },
   {
     //Quote 6
-    author: 'Bob Marley' ,
-      quote:'The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.',
-      number:'celeberities'
+    source: 'Ben Parker' ,
+      quote:'With great power, comes great responsibility.',
+      citation: 'Marvel Comics: The Amazing Spider-Man' ,
+      tag:'comics'
   },
   {
     //Quote 7
-    author: 'Jesus Christ' ,
+    source: 'Jesus Christ' ,
       quote:'For what shall it profit a man, if he gain the whole world, and suffer the loss of his soul?',
-      number:'religious'
+      tag:'religious'
   },
   {
     //Quote 8
-    author: 'David Hasselhoff' ,
+    source: 'David Hasselhoff' ,
       quote:'I am a cheesy over-the-top megalomaniac with a deep voice and the most amazing pecs.',
-      number:'celeberities'
+      tag:'celeberities'
   },
   {
     //Quote 9
-    author: 'George W. Bush' ,
+    source: 'George W. Bush' ,
       quote:'Some folks look at me and see a certain swagger, which in Texas is called "walking."',
-      number:'politics'
+      tag:'politics'
   },
   {
     //Quote 10
-    author: 'Donald Trump' ,
+    source: 'Donald Trump' ,
       quote:'No dream is too big. No challenge is too great. Nothing we want for our future is beyond our reach.',
       year: 'Nov 9, 2016',
-      number:'politics'
+      tag:'politics'
 
   },
   {
     //Quote 11
-    author: 'Britney Spears' ,
+    source: 'Britney Spears' ,
       quote:'The cool thing about being famous is traveling. I have always wanted to travel across seas, like to Canada and stuff.',
-      number:'celeberities'
+      tag:'celeberities'
   },
   {
     //Quote 12
-    author: 'Barack Obama' ,
+    source: 'Barack Obama' ,
       quote: 'If you are walking down the right path and you are willing to keep walking, eventually you will make progress.',
-      number:'politics'
+      tag:'politics'
   }
 ];
-
+//Log array to console to test 1st stage of code and check for errors.
 console.log(quotes);
+//End of Part 1 coding project: console test positive
 
-var message;
-var citation;
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Use the random number to `return` a random quote object from the `quotes` array.
-***/
-
+//Coding project part 2
+//Function random quote uses a random number to select a quote form the object array.
 function getRandomQuote(){
   //random number generator from zero to length of array
     var randomNum = Math.floor(Math.random() * quotes.length );
-//Array loop
-for ( var i=0; i<quotes.length; i++ ){
-//var grabQuote should return one of the quotes from the array by concatenating array + randomNum
+//var grabQuote returns one of the quotes from the array by concatenating array + randomNum
     var grabQuote = quotes[randomNum] ;
   return grabQuote;}
-}
 
+//Tests random quote generator in console
 console.log(getRandomQuote());
+//End of part 2 coding project: console test positive
 
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
 
+//coding project part 3
+//Function grabs random quote by calling the quote function and then printing it with an HTML string.
 function printQuote(){
   var randomQuote = getRandomQuote();
+//Change background color with each new quote.
+  prettyColors();
+  //empty var for the html message
   var html= '';
-  html+='<h2>'+ randomQuote +'</h2>';
+  //using 'class' and 'id' to use the needed CSS styles and placement.
+  html+='<p class="quote" id="quote">'+ randomQuote.quote +' </p><br>';
+  html+='<p class="source" >'+ randomQuote.source + '  <br>';
+
+//'if' conditional statments for year, cittion, and tag when needed
+  if(randomQuote.year)
+  {html+='<span class="year"> date: ' + randomQuote.year + ' </span><br>';}
+  if(randomQuote.citation){
+    html+='<span class="citation"> ' + randomQuote.citation + ' </span><br>';}
+  if(randomQuote.tag){
+    html+='<span class="tag">' + randomQuote.tag + ' </span>';}
+    html+= '</p>';
+
+  var div = document.getElementById('quote-box');
+    div.innerHTML = html;
+
 }
 
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+/// Dont Touch This Code!
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//color change function  'common code from TreeHouse workshop, MDN, and other code sources'
+function prettyColors(){
+  red = Math.floor(Math.random() * 256 );
+  blue = Math.floor(Math.random() * 256 );
+  green = Math.floor(Math.random() * 256 );
+  randomColor =  'rgb('+ red+ ',' + blue+ ',' + green +')';
+//Changes the CSS background color each time the button is clicked
+  document.body.style.backgroundColor = randomColor;
+}
+
+setInterval(printQuote, 20000);
